ddd0f92c5f7c3bbc787a97faa7459f3c1facac5d
ylber-gashi <ylber.gashi@studentet.uni-pr.edu>
2020-04-15 19:34:01 +0200

    near final

--- DetyraA.s
+++ DetyraA.s
@@ -4,25 +4,23 @@ jepNrAnt: .asciiz "Jep numrin e anetareve te vektorit (max. 5): "
 shtypElem: .asciiz "Shtyp elementet nje nga nje: \n"
 vleratEvekt: .asciiz "Vlerat e vektorit jane: \n"
 endl: .asciiz "\n"
+n: .word 0
 
 .text
     main:
     la $a0, array
-    addi $a1, $zero, 0  #inicializimi i n-it (pra ne a1)
+    lw $a1, n
 
     jal populloVektorin  #thirret funksioni
     #move $t0, $v1   #ne t0 po e ruajme n-in e cila eshte dhene ne funksionin populloVektorin
     
+    
+    la $a0, array 
     move $a1, $v1
-    la $a0, array
+    jal unazaKalimit
 
-    jal unazakalimit
-    
-
-    
-    exit:
-        li $v0, 10
-        syscall
+    li $v0, 10
+    syscall
 
 
 populloVektorin:
@@ -67,9 +65,9 @@ populloVektorin:
         jr $ra
         
 
-unazakalimit:  
-    addi $sp, $sp, -12
-    sw $ra, 0($sp)
+unazaKalimit:  
+    addi $sp, $sp, -4
+    sw $ra, 0($sp)      #adresen kthyese te ketij funksioni e ruajme ne stack
 
     move $s0, $a0  #array
     move $s1, $a1   #vlera e n
@@ -77,25 +75,59 @@ unazakalimit:
     li $t0, 0
     
     loop1: 
-        bgt $t0, $s1, stop
+        beq $t0, $s1, endloop1
         
         li $t3, 4
-        mul $t8, $t0, $t3
-        add $t6, $s0, $t8 #adresen e array po e inkrementojme per vleren e $t8, e cila shumezohet me 4 pas cdo iterimi
+        mul $t4, $t0, $t3
+        add $t5, $s0, $t4 #adresen e array po e inkrementojme per vleren e $t8, e cila shumezohet me 4 pas cdo iterimi
 
-        lw $t1, 0($t6) #variabla min
-        move $t2, $t0 #loc
+        lw $t1, 0($t5) #variabla min
+        addi $t2, $t0, 0 #loc
         
-        move $a2, $t0   #parametri p qe do ti jipet funksionit ne vazhdim
-        move $a3, $t1   #parametri min
-
-        sw $t2, 4($sp)  #po e ruajme ne stack loc per me e shfrytezu si parameter
+        addi $a2, $t0, 0  #parametri p qe do ti jipet funksionit ne vazhdim
+        addi $a3, $t1, 0   #parametri min
+        addi $sp, $sp, -8
+        sw $t2, 0($sp)  #po e ruajme ne stack loc per me e shfrytezu si parameter
 
         jal unazavlerave
-        j loop1
-    stop:
-        lw $ra, 0($sp)
+        lw $t1, 0($sp)
+        lw $t2, 4($sp)
+        addi $sp, $sp, 8
 
+        li $t3, 4
+        mul $t4, $t0, $t3 #$t3 e kemi deklaru ma nalt si 4, se na duhet per me nxjerr elemente nga array(si shumefisha te 4)
+        add $t5, $s0, $t4 #qeshtu e inkrementojme adresen per casjen e elementeve te array
+
+        lw $t8, 0($t5) #temp = a[p]
+        sw $s7, 0($t5) #a[p] = a[loc]
+
+        mul $t6, $t2, $t3
+        add $t4, $s0, $t6 #loc si shumefish i 4
+        sw $t8, 0($t4) #a[loc] = temp
+        addi $t0, $t0, 1
+        j loop1
+    endloop1:
+        lw $ra, 0($sp)
+        addi $sp, $sp, 4
+
+    li $t0, 0
+
+    printimi:
+        bgt $t0, $s1, fuckinEXIT
+
+        li $v0, 1
+        lw $a0, 0($s0)
+        syscall 
+
+        addi $s0, $s0, 4 #se adresat e anetareve jane shumefish i 4
+        addi $t0, $t0, 1
+
+        li $v0, 4
+        la $a0, endl        #rresht i ri
+        syscall
+
+        j printimi
+    fuckinEXIT:
         jr $ra
 
 
@@ -104,10 +136,31 @@ unazavlerave:
     move $s4, $a1 #n 
     move $s5, $a2 #p
     move $s6, $a3 #min
-    lw $s7, 4($sp) #loc
+    lw $s7, 0($sp) #loc
 
-    jr $ra
+    addi $t6, $s5, 1 #p + 1 = k
 
+    loop2:
+        bgt $t6, $s4, endloop2
+        li $t4, 4
+        mul $t5, $t6, $t4
+        add $t4, $s3, $t5
+
+        lw $t7, 0($t4)  #po e marrim nje anetar nga array si min
+
+        blt $s6, $t7, pass
+        move $s6, $t7   #min = a[k]
+        move $s7, $t6   #loc = k
+        sw $s6, 0($sp)
+        sw $s7, 4($sp)
+        addi $t6, $t6, 1 #po edhe nese nuk gjindet su bo nomi, kalom ne iterimin tjt
+            j loop2
+        pass:
+            addi $t6, $t6, 1 #po edhe nese nuk gjindet su bo nomi, kalom ne iterimin tjt
+            j loop2
+    endloop2:
+        jr $ra
+    
 
 
 
